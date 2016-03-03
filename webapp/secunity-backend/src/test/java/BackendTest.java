7f847b855796de3745515e5608f5d857f6562d0f
Julian Schuette <julian.schuette@aisec.fraunhofer.de>
2016-03-03 09:06:51 +0100

    unit tests++

--- webapp/secunity-backend/src/test/java/BackendTest.java
+++ webapp/secunity-backend/src/test/java/BackendTest.java
@@ -1,13 +1,18 @@
+import static org.junit.Assert.assertEquals;
+
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Response;
 
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Before;
 import org.junit.Test;
 
+import de.fhg.aisec.secunity.db.TripleStore;
 import de.fhg.aisec.secunity.rest.InstitutionResource;
 
 public class BackendTest extends JerseyTest {
@@ -17,16 +22,29 @@ public class BackendTest extends JerseyTest {
 	        return new ResourceConfig(InstitutionResource.class);
 	    }
 
+	    @Before
+	    public void cleanDB() {   	
+	    	// Clear database
+	    	TripleStore.getInstance().deleteAll();
+	    }
+	    
 	    @Test
 	    public void testCreate() {
-//	        String hello = target("institution/0").request().get(String.class);
+	    	// Create new entry
 	    	Map<String, String> data = new HashMap<String, String>();
 	    	data.put("name", "Fraunhofer AISEC");
 	    	data.put("city", "Garching near Munich");
 	    	data.put("topic", "Hardware Security");
 	    	data.put("topic", "Software Security");
-	        String hello = target("institution").request().post(Entity.json(data), String.class);
-	        System.out.println(hello);
+	        Response resp = target("institution").request().post(Entity.json(data), Response.class);
+	        assertEquals(200, resp.getStatus());
+	        
+	        // Retrieve entry
+	        resp = target("institution/Fraunhofer AISEC").request().get(Response.class);
+	        System.out.println(resp.getEntity().toString());
+	        assertEquals(Response.Status.OK, resp.getStatus());
+
 	    }
+	
 	
 }
